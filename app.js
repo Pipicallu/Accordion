a4b5a8b7f8bf8e510ad3db081afa509e0e5c4520
unknown <thomas.vassallo@gmail.com>
2021-07-09 21:23:00 +0100

    removed css transition as well as second for each loop as transitions were clunky, changed JS to run everything in one for each loop

--- app.js
+++ app.js
@@ -4,17 +4,20 @@ var questions = document.querySelectorAll(".accordion-button");
 // forEach allows us to access each element within the array.
 questions.forEach((question) => {
     question.addEventListener("click", () =>{
-        if(question.classList.contains("opened-accordion")){
-            question.classList.remove("opened-accordion")
-        }else{
+            // checks if there are any opened accordions
+            const openedAccordion = document.querySelector(".accordion-button.opened-accordion");
+            // checks to make sure the opened accordion is not equal to clicked element
+            if (openedAccordion && openedAccordion!==question){
+                openedAccordion.classList.toggle("opened-accordion");
+                openedAccordion.nextElementSibling.style.maxHeight = 0;
+            }
             
-            // checks to see which elements already have the opened class
-            const questionsOpened = document.querySelectorAll(".opened-accordion");
-            // proceeds to remove any instances of the remained class.
-            questionsOpened.forEach((questionOpen) =>{
-                questionOpen.classList.remove("opened-accordion");
-            })  
-            question.classList.add("opened-accordion"); 
+            question.classList.toggle("opened-accordion");
+            const accordionContent = question.nextElementSibling;
+            if (question.classList.contains("opened-accordion")){
+                accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
+        }else{
+            accordionContent.style.maxHeight = 0; 
         }
     })
     
